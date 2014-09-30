01e287801c7cc64a27ef920ec11d07b40e85321e
elnazd <elnaz.delpisheh@gmail.com>
2014-09-30 13:30:35 -0400

    Debugging

---
+++ p6.cpp
@@ -0,0 +1,48 @@
+/*Find the syntax error in the following classes*/
+
+a.
+class AA
+{
+public:
+	void print() const;
+	int sum();
+	AA();
+	int AA(int,int);
+private:
+	int x;
+	int y;
+}
+/*Answer:
+
+*/
+
+b.
+class BB
+{
+	int one;
+	int two;
+public:
+	bool equal() const;
+	print();
+	BB(int,int);
+}
+/*Answer:
+
+*/
+
+c.
+class CC
+{
+public:
+	void set(int,int);
+	void print() const;
+	CC();
+	CC(int,int);
+	bool CC(int,int);
+private:
+	int x;
+	int y;
+}
+/*Answer:
+
+*/
