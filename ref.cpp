d9b83845f36e7eb36cd4e5fd0631c38b26dd72a7
elnazd <elnaz.delpisheh@gmail.com>
2014-10-05 22:23:44 -0400

    Week 4-References Review

---
+++ ref.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main()
+{
+	int intOne;
+	int &rSomeRef = intOne;
+	
+	intOne = 5;
+	cout << "intOne: " << intOne << endl;
+	cout << "rSomeRef: " << rSomeRef << endl;
+	
+	rSomeRef = 7;
+	cout << "intOne: " << intOne << endl;
+	cout << "rSomeRef: " << rSomeRef << endl;
+	return 0;
+}
